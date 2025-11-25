155f550f3bf1043f4e879f2781032265dc10301e
Siddhesh Z <siddheshzope1023@gmail.com>
2025-11-25 16:09:50 +0530

    get by id service

--- app/api/v1/endpoints/service.py
+++ app/api/v1/endpoints/service.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import Annotated, List
 from app.schemas.service import ServiceCreate, ServiceResponse
-from app.services.service import create_service, get_services
+from app.services.service import create_service, get_services, get_service_by_id
 from app.db.session import get_db
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -14,19 +14,16 @@ security = HTTPBearer()
 def add_service(service: ServiceCreate, db: Session = Depends(get_db)):
     return create_service(db, service)
 
-# @router.get("/", response_model=List[ServiceResponse])
-# def list_services(credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-#     if not credentials:
-#         raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail="Cred missing")
-    
-#     token  = credentials.credentials
-#     payload = decode_access_token(token)
-
-#     if not payload:
-#         raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail="INvalid or expired token missing")
-    
-#     return get_services(db, skip, limit)
-
 @router.get("/", response_model=List[ServiceResponse],dependencies=[Depends(JWTBearer())])
-def list_services(credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def list_services(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return get_services(db, skip, limit)
+
+@router.get("/{id}", response_model=List[ServiceResponse],dependencies=[Depends(JWTBearer())])
+def get_service(id,credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return get_service_by_id(id,db,skip,limit)
+
+@router.put("/{id}", response_model=List[ServiceResponse],dependencies=[Depends(JWTBearer())])
+def update_service(id,credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return get_service_by_id(id,db,skip,limit)
+
+
--- app/services/service.py
+++ app/services/service.py
@@ -11,3 +11,11 @@ def create_service(db: Session, service: ServiceCreate):
 
 def get_services(db: Session, skip: int = 0, limit: int = 10):
     return db.query(Service).offset(skip).limit(limit).all()
+
+
+def get_service_by_id(id,db: Session, skip: int = 0, limit: int = 10):
+    service = db.query(Service).filter(Service.id == id).first()
+    # if service is None:
+    #     raise HTTPException(status_code=404,detail="User not found")
+    return service
+
